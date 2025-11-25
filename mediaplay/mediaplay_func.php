a0de912b5f95355cf360cd664d9d1e996f08bdec
amargozz <malexgope@gmail.com>
2025-11-25 16:53:30 -0600

    Update mediaplay_func.php
    adding comments

--- mediaplay/mediaplay_func.php
+++ mediaplay/mediaplay_func.php
@@ -1,8 +1,9 @@
 <?php
-function getMedia($folder = 'KIOSK_MEDIA') {
+function getMedia($folder = 'KIOSK_MEDIA') {                                     //Path of directory
     $imagenes = glob("$folder/*.{jpg,jpeg,png,PNG,gif,bmp,webp}", GLOB_BRACE);
     $videos = glob("$folder/*.{mp4,webm,ogg}", GLOB_BRACE);
     return array_merge($imagenes, $videos);
 }
 ?>
+
 
